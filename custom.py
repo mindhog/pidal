1d3d4d31b9ec9764a4d82580fd00fb002fe1c5c3
Michael Muller <mmuller@google.com>
2020-10-27 19:50:08 -0400

    Add double-press to get to config list, fix some volumes, add a first implementation of "ConfigFramework"

--- custom.py
+++ custom.py
@@ -3,7 +3,9 @@ import abc
 from midi import ControlChange, ProgramChange
 from subprocess import Popen
 import threading
+from typing import Callable
 from engine import Config, Engine, ProcessManager
+from util import Actuator, ConfigFramework, FlagSetController
 
 print('in custom')
 
@@ -27,6 +29,36 @@ class RadioController:
         engine.notify('pedal_button_status', self.active, False)
         engine.notify('pedal_button_status', index, True)
         self.active = index
+
+def double_press(func: Callable[[bool], None],
+                 alt_func: Callable[[bool], None],
+                 other: int
+                 ) -> Callable[[bool], None]:
+    """Returns a footswitch-press handler to handle simultaneous depress of two
+    footswitches.
+
+    Returns a handler that calls alt_func() if footswitch 'other' is also
+    depressed and calls func() if not.  This has the effect of binding the
+    press of both footswitches to alt_func.
+
+    Note that this isn't perfect, as triggering the first footswitch will
+    still result in a "pressed" event being passed through to func().
+    Furthermore, all footswitch released events will continue to be passed to
+    func.
+    """
+    def handler(pressed: bool) -> None:
+        if pressed:
+            engine = Engine.get_instance()
+            if engine.is_fs_pressed(other):
+                alt_func(pressed)
+                return
+        func(pressed)
+
+    return handler
+
+def show_config_list(pressed: bool) -> None:
+    if pressed:
+        engine.notify('config_list')
 
 def make_program_switcher(controller: RadioController, bank, program) -> None:
     def switcher(pressed: bool) -> None:
@@ -76,7 +108,7 @@ class FirstConfig(Config):
 
     def on_enter(self):
         engine.jack_connect('system:capture_1', 'gx_head_amp:in_0')
-        engine.set_program(gtx_port, 0, 0)
+        engine.set_program(gtx_port, 1, 0)
         for bank, prog in ((1, 0), (1, 1), (1, 2), (1, 3)):
             engine.register_footswitch(
                 prog,
@@ -98,10 +130,10 @@ class GuitarixSimple(Config):
         if pressed:
             self.states[cc] = not self.states[cc]
             engine.seq.sendEvent(ControlChange(0, 0, cc,
-                                            0x7f if self.states[cc] else 0
-                                            ),
-                                gtx_port
-                                )
+                                               0x7f if self.states[cc] else 0
+                                               ),
+                                 gtx_port
+                                 )
             engine.notify('pedal_button_status', index, self.states[cc])
 
     def on_enter(self):
@@ -109,8 +141,25 @@ class GuitarixSimple(Config):
         engine.set_program(gtx_port, 0, 0)
         engine.register_footswitch(0, lambda x: self.switch(x, 13, 0))
         engine.register_footswitch(1, lambda x: self.switch(x, 14, 1))
-        engine.register_footswitch(2, lambda x: self.switch(x, 11, 2))
-        engine.register_footswitch(3, lambda x: self.switch(x, 12, 3))
+        engine.register_footswitch(
+            2,
+            double_press(lambda x: self.switch(x, 11, 2),
+                         show_config_list,
+                         3
+                         )
+        )
+        engine.register_footswitch(
+            3,
+            double_press(lambda x: self.switch(x, 12, 3),
+                         show_config_list,
+                         2
+                         )
+        )
+
+        # Reset all of the pedals.
+        for cc in (11, 12, 13, 14):
+            engine.seq.sendEvent(ControlChange(0, 0, cc, 0), gtx_port)
+        self.states = {11: False, 13: False, 14: False, 12: False}
 
     def on_leave(self):
         engine.jack_disconnect_all('gx_head_amp:in_0', False)
@@ -163,14 +212,14 @@ class RakBizConfig(RakConfig):
         self.buttons = ['Dist', 'Chor', 'Wah', 'Out']
         self.states = 0  # bits 1, 2, and 3 are the pedal states.
         self.map = [
-            (0, 23, 64),
+            (0, 23, 70),
             (0, 6, 64),
             (2, 59, 64),
             (0, 52, 64),
             (0, 33, 86),
             (0, 43, 78),
             (2, 40, 77),
-            (2, 7, 82),
+            (2, 7, 64),
         ]
 
     def __on_press(self, fs: int, bit: int, pressed: bool):
@@ -181,18 +230,16 @@ class RakBizConfig(RakConfig):
             engine.seq.sendEvent(ControlChange(0, 0, 7, vol), rak_port)
             engine.notify('pedal_button_status', fs, bool(self.states & bit))
 
-    def __on_exit(self, pressed: bool):
-        """Called when the "exit footswitch" is pressed."""
-        if pressed:
-            engine.notify('config_list')
-
     def set_presets(self):
         for fs in range(3):
             engine.register_footswitch(
                 fs,
                 lambda p, fs=fs, bit=1 << fs: self.__on_press(fs, bit, p)
             )
-        engine.register_footswitch(3, self.__on_exit)
+        engine.register_footswitch(3, show_config_list)
+        bank, prog, vol = self.map[0]
+        engine.set_program(rak_port, bank, prog)
+        engine.seq.sendEvent(ControlChange(0, 0, 7, vol), rak_port)
 
 class RakFunConfig(RakConfig):
     def __init__(self):
@@ -207,12 +254,50 @@ class RakFunConfig(RakConfig):
         for (fs, prog) in enumerate([6, 29, 32, 8]):
             engine.register_footswitch(fs,
                                        self.make_program_switcher(0, prog, fs))
+        engine.register_footswitch(
+            2,
+            double_press(self.make_program_switcher(0, 32, 2),
+                         show_config_list,
+                         3
+                         )
+        )
+        engine.register_footswitch(
+            3,
+            double_press(self.make_program_switcher(0, 8, 3),
+                         show_config_list,
+                         2
+                         )
+        )
         self.controller.activate(engine, 0)
+        engine.set_program(rak_port, 0, 6)
+
+def act(program: int) -> None:
+    def enable():
+        engine.set_program(rak_port, 3, program)
+    return Actuator(enable, lambda: None)
+
+class NewConfig(ConfigFramework):
+
+    def __init__(self):
+        super().__init__(
+            'New Config',
+            FlagSetController(['1', '2', '3', '4'],
+                              [act(p) for p in range(1, 17)]
+                              )
+        )
+
+    def on_enter(self):
+        engine.midi_connect('pidal/to_rak',
+                            'rakarrack-plus/rakarrack-plus IN')
+
+        engine.jack_connect('system:capture_1', 'rakarrack-plus:in_1')
+        engine.jack_connect('system:capture_1', 'rakarrack-plus:in_2')
 
 simple = GuitarixSimple()
 engine.set_config(simple)
 engine.add_config(simple)
 engine.add_config(FirstConfig())
+engine.add_config(NewConfig())
 
 # Wait for rakarrack asynchronously (since for some reason it takes a really
 # long time to start) and then register the configs for it.
